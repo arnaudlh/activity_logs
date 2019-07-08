48732f5aa594549cce0fd4dc266cffb5fe24fedd
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-07-08 10:53:39 +0800

    Export object

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 ##export the logging details for EH and SA
+/*
 output "eh_logs_id" {
   depends_on = [azurerm_eventhub_namespace.log]
 
@@ -16,4 +17,20 @@ output "sa_logs_id" {
 
   value = azurerm_storage_account.log.id
 }
+*/
 
+output "seclogs_map" {
+    depends_on = [
+        azurerm_storage_account.log, 
+        azurerm_eventhub_namespace.log,
+        azurerm_eventhub_namespace.log
+        ]
+
+    value = "${
+        map(
+            "activity_sa", "${azurerm_storage_account.log.id}",
+            "activity_eh_name",  "${azurerm_eventhub_namespace.log.name}",
+            "activity_eh_id", "${azurerm_eventhub_namespace.log.id}"
+        )
+    }"
+}
